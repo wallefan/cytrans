e08520887a5920162e6037fa0504d1959ef6832b
Sean <>
2023-03-25 19:07:54 -0700

    Multiple audio tracks should be fixed. Hopefully.

--- examples/extract.rs
+++ examples/extract.rs
@@ -6,12 +6,27 @@ use serde_json::to_writer;
 use std::fs::{OpenOptions, create_dir};
 
 fn main() {
-    let file = Path::new("test.mkv");
-    let ffprobe = ffprobe(file).expect("ffprobe error");
-    let outputdir = Path::new("extracted");
-    let (mut command, cytube_data) = remux(file, &ffprobe, outputdir, "https://red.baka.haus/panzer/", Some("eng".into()));
+    let mut args = std::env::args_os();
+    let argv0 = args.next().unwrap(); // skip argv0
+    if args.len() != 4 {
+        eprintln!("usage: {} <input file> <output directory> <URL prefix>", argv0.to_string_lossy());
+    }
+    let file = args.next().unwrap();
+    let outputdir = args.next().unwrap();
+    let urlprefix = args.next().unwrap();
+    
+    let file = Path::new(&file);
+    let outputdir = Path::new(&outputdir);
+    let urlprefix = urlprefix.to_string_lossy();
 
-    let _ = create_dir(outputdir);
+    let ffprobe = ffprobe(file).expect("ffprobe error");
+    let (mut command, cytube_data) = remux(file, &ffprobe, outputdir, &urlprefix, Some("eng".into()));
+
+    if let Err(e) = create_dir(outputdir) {
+        if e.kind() != std::io::ErrorKind::AlreadyExists {
+            panic!("error creating the output directory: {}", e);
+        }
+    }
 
     {
         let f = OpenOptions::new().write(true).create(true).truncate(true).open(outputdir.join("manifest.json")).expect("could not open JSON file for writing");
--- src/transcode.rs
+++ src/transcode.rs
@@ -4,6 +4,7 @@ use crate::ffmpeg_languages::*;
 use std::path::Path;
 use std::process::Command;
 use fixedstr::str4;
+use std::collections::HashMap;
 
 const BITMAP_SUBTITLE_CODECS: [&'static str; 4] = [
     "dvb_subtitle",
@@ -148,87 +149,59 @@ pub fn remux(media_file: &Path, ffprobe: &FFprobeResult, outputdir: &Path, url_p
 
     let mut command = Command::new("ffmpeg");
     command.arg("-hide_banner");
-    command.args(["-strict", "-2"]);
     command.arg("-i").arg(media_file.as_os_str());
 
     let mut ct_sources = Vec::new();
     let mut ct_audio_tracks = Vec::new();
     let mut ct_text_tracks = Vec::new();
+
+    // okay so fun fact
+    // if the main video file contains a muxed audio track
+    // and the manifest also contains a supplemental audio track
+    // rather than letting the user pick one
+    // the browser will play them both simultaneously
+    // so what we need to do
+    // if there is only one audio track, mux it into the file
+    // if there is more than one audio track, generate a silent audio track, mux that into the
+    // file, and split out each audio as its own separate file.
+    
+    // if there's a way to do this idiomatically and declaratively i'd love to hear about it
+    let mut audio_tracks_by_language = HashMap::<str4, Vec<&Track>>::new();
+    for track in audio_tracks.iter() {
+        audio_tracks_by_language.entry(track.language.unwrap_or("".into()))
+            .or_default()
+            .push(*track);
+    }
     
     if let Some(video) = video_tracks.first() {
         let video_container = find_video_container(&video.codec);
-        let mut chosen_audio = audio_tracks.first();
-        let mut highest_score = 0;
-        for audio in audio_tracks.iter() {
-            let mut score = 0;
-            if video_container.as_ref().map_or(true, |container| container.get_acceptable_audio_codecs().contains(&audio.codec.as_str())) {
-                score += 1;
-            }
-            if preferred_language.as_ref().map_or(true, |lang| audio.language == Some(*lang)) {
-                score += 1;
-            }
-            if score > highest_score {
-                chosen_audio = Some(audio);
-                highest_score = score;
-            }
-        }
-        if let Some(audio) = chosen_audio {
-            command.args(["-map",
-                         format!("0:{}", video.index).as_str(),
-                         "-map",
-                         format!("0:{}", audio.index).as_str(),
-            ]);
-            if let Some(video_container) = video_container {
-                command.args([
-                             "-c:v", "copy",
-                             "-c:a",
-                ]);
-                if video_container.get_acceptable_audio_codecs().contains(&audio.codec.as_str()) {
-                    command.arg("copy");
-                    if matches!(video_container, VideoContainer::MP4) && audio.codec == "flac" {
-                        // ffmpeg doesn't like putting FLAC streams inside MP4 files, considers it
-                        // experimental.  we have to tell it that that's okay
-                        command.args(["-strict", "experimental"]);
-                    }
-                } else {
-                    command.args([video_container.preferred_audio_encoder(),
-                                  "-ac", "2"]); // downmix to stereo to make encoding faster
+
+        let (audio_track, audio_source) = if audio_tracks_by_language.len() == 1 {
+            // one audio language.  mux it into the video.
+            let mut chosen_audio = audio_tracks.first().unwrap();
+            let mut highest_score = 0;
+            for audio in audio_tracks.iter() {
+                let mut score = 0;
+                if video_container.as_ref().map_or(false, |container| container.get_acceptable_audio_codecs().contains(&audio.codec.as_str())) {
+                    score += 100;
                 }
-
-                let filename = format!("main.{}", video_container.extension());
-
-                command.arg(outputdir.join(&filename));
-                ct_sources.push(Source{
-                    bitrate: ffprobe.bitrate,
-                    content_type: video_container.mimetype(),
-                    quality: video.scanline_count.unwrap(), // TODO
-                    url: strcat(url_prefix, &[filename.as_str()]),
-                });
-            } else {
-                // the codec used in the original video file isn't supported by the browser
-                // AV1 transcode it is
-                command.args(["-c:v", "libstvav1", "-c:a", "libopus", "-ac", "2"]);
-                command.arg(outputdir.join("main.webm"));
-                ct_sources.push(Source{
-                    bitrate: ffprobe.bitrate, // TODO figure out the actual bitrate
-                    content_type: "video/webm",
-                    quality: video.scanline_count.unwrap(), // TODO
-                    url: strcat(url_prefix, &["main.webm"]),
-                });
-            }
-
-            for audio_track in audio_tracks.iter() {
-                if audio_track.index == audio.index {
-                    // no point generating a separate file for the audio track we muxed into the
-                    // video file.
-                    continue;
+                // TODO sort audio tracks by channel count!
+                if score > highest_score {
+                    chosen_audio = audio;
+                    highest_score = score;
                 }
-
+            }
+            (Some(chosen_audio), format!("0:{}", chosen_audio.index))
+        } else {
+            // multiple audio languages.  break out each into its own audio file and embed silence
+            // into the muxed video.
+            for (language, audio_tracks) in audio_tracks_by_language.iter() {
+                let language = language.as_str();
+                let audio_track = audio_tracks.first().unwrap(); // TODO choose an audio track more
+                                                                 // intelligently than this.
                 if let Some(container) = find_audio_container(&audio_track.codec) {
-                    let language = audio_track.language.as_ref().map(|x| x.as_str()).unwrap_or("unknown");
-
                     let filename = format!("audio_{}_{}.{}", audio_track.index, language, container.extension());
-                    
+
                     command.arg("-map");
                     command.arg(format!("0:{}", audio_track.index));
                     command.args(["-c", "copy"]);
@@ -240,12 +213,63 @@ pub fn remux(media_file: &Path, ffprobe: &FFprobeResult, outputdir: &Path, url_p
                         label: build_language_string(&language, audio_track.title.as_ref().map(|x|x.as_str())),
                         url: strcat(url_prefix, &[&filename]),
                     });
-                }
-                
-
+                } // TODO transcode additional audio tracks.
             }
-        }
+            // TODO copy the sample rate and channel layout from the source file!
+            command.args(["-f", "lavfi", "-t", ffprobe.duration.to_string().as_str(), "-i", "anullsrc=channel_layout=stereo:sample_rate=48000",
+            ]);
+            (None, "1:0".to_string())
+        };
+        command.args([
+                     "-map",
+                     format!("0:{}", video.index).as_str(),
+                     "-map", &audio_source,
+        ]);
 
+        if let Some(video_container) = video_container {
+            command.args([
+                         "-c:v", "copy",
+                         "-c:a",
+            ]);
+            if let Some(audio) = audio_track {
+                if video_container.get_acceptable_audio_codecs().contains(&audio.codec.as_str()) {
+                    command.arg("copy");
+                    if matches!(video_container, VideoContainer::MP4) && audio.codec == "flac" {
+                        // ffmpeg doesn't like putting FLAC streams inside MP4 files, considers it
+                        // experimental.  we have to tell it that that's okay
+                        command.args(["-strict", "experimental"]);
+                    }
+                } else {
+                    command.args([video_container.preferred_audio_encoder(),
+                    "-ac", "2"]); // downmix to stereo to make encoding faster
+                }
+            } else {
+                // above code has elected not to embed an audio track in the file.
+                // all we're encoding is silence so codec doesn't particularly matter.
+                command.arg(video_container.preferred_audio_encoder());
+            }
+
+            let filename = format!("main.{}", video_container.extension());
+
+            command.arg(outputdir.join(&filename));
+            ct_sources.push(Source{
+                bitrate: ffprobe.bitrate,
+                content_type: video_container.mimetype(),
+                quality: video.scanline_count.unwrap(), // TODO
+                url: strcat(url_prefix, &[filename.as_str()]),
+            });
+        } else {
+            // the codec used in the original video file isn't supported by the browser
+            // AV1 transcode it is
+            command.args(["-c:v", "libstvav1", "-c:a", "libopus", "-ac", "2"]);
+            command.arg(outputdir.join("main.webm"));
+            ct_sources.push(Source{
+                bitrate: ffprobe.bitrate, // TODO figure out the actual bitrate
+                content_type: "video/webm",
+                quality: video.scanline_count.unwrap(), // TODO
+                url: strcat(url_prefix, &["main.webm"]),
+            });
+        }
     }
 
     for sub_track in subtitle_tracks {
@@ -273,6 +297,7 @@ pub fn remux(media_file: &Path, ffprobe: &FFprobeResult, outputdir: &Path, url_p
         });
     }
 
+    dbg!(&command);
     (command,
     CytubeVideo {
         title: ffprobe.title.clone().unwrap_or_else(|| media_file.file_stem().unwrap().to_string_lossy().to_string()),
